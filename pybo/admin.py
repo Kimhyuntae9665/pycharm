1269ceaa561e2b9b6d60f1e9a4d8fe1d1771164f
Kimhyuntae9665 <Kimhyuntae9665>
2023-06-22 12:08:48 +0900

    Django Admin을 추가

--- pybo/admin.py
+++ pybo/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Question
+
+# QuestionAdmin 클래스를 생성하고 제목 검색을 위해 search_fields 속성에 subject를 추가 했다
+#검색 기능이 추가된 화면 구현
+class QuestionAdmin(admin.ModelAdmin):
+    search_fields=['subject']
+
+admin.site.register(Question,QuestionAdmin)
